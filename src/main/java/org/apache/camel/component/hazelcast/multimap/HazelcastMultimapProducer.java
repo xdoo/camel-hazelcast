ad2c1144798591b5028e5d74a24008d103bb56fc
Claus Straube <info@catify.com>
2011-02-18 09:48:36 +0100

    added apache license headers

--- src/main/java/org/apache/camel/component/hazelcast/multimap/HazelcastMultimapProducer.java
+++ src/main/java/org/apache/camel/component/hazelcast/multimap/HazelcastMultimapProducer.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.hazelcast.multimap;
 
 import java.util.Map;
@@ -9,7 +25,6 @@ import org.apache.camel.component.hazelcast.HazelcastConstants;
 import org.apache.camel.impl.DefaultProducer;
 
 import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.IMap;
 import com.hazelcast.core.MultiMap;
 
 public class HazelcastMultimapProducer extends DefaultProducer {
